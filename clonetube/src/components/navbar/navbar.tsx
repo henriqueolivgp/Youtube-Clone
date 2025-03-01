c9cf3902d639851fa926ca6466252c3dd42c1c26
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-01 16:06:12 +0000

    Add all components(MenuButton, Logo, Search, Mic, Create, Notifications, UserMenu

--- clonetube/src/components/navbar/navbar.tsx
+++ clonetube/src/components/navbar/navbar.tsx
@@ -1,13 +1,29 @@
-
+import { MenuButton } from "./buttons/menu/menu";
+import { Logo } from "./logo/logo";
+import { Search } from "./search/search";
+import { Mic } from "./buttons/mic/mic";
+import { Create } from "./buttons/Create/create";
+import { Notifications } from "./buttons/notifications/notifications";
+import { UserMenu } from "./buttons/user/user";
 
 export const Navbar = () => {
-  return(
+  return (
     <>
-    <nav className="flex w-full h-20 p-2 bg-amber-50">
-      <section>
-
-      </section>
-    </nav>
+      <nav className="flex w-full h-14 p-3 items-center justify-between bg-white dark:bg-neutral-900">
+        <section className="flex gap-6 ml-2">
+          <MenuButton />
+          <Logo />
+        </section>
+        <section className="relative flex justify-center items-center gap-4 w-full">
+          <Search />
+          <Mic />
+        </section>
+        <section className="flex justify-center items-center gap-4 mr-4">
+          <Create />
+          <Notifications />
+          <UserMenu />
+        </section>
+      </nav>
     </>
-  )
-}
+  );
+};
