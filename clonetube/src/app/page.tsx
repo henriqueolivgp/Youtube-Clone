d6790323c8144dc3f11c7699bc471a86616a02c0
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-01 15:59:30 +0000

    add some styles for responsive page

--- clonetube/src/app/page.tsx
+++ clonetube/src/app/page.tsx
@@ -1,8 +1,9 @@
 
+
 export default function Home() {
   return (
     <>
-    <section className="flex justify-center items-center">
+    <section className="flex justify-center items-center ">
       <h1>Hello</h1>
     </section>
     </>
