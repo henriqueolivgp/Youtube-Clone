4a6a89d5cd73fe79c02d850d09e94337835d8cff
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-11 09:20:39 +0000

    readjustment in navbar responsivity

--- clonetube/src/components/navbar/navbar.tsx
+++ clonetube/src/components/navbar/navbar.tsx
@@ -9,12 +9,12 @@ import { UserMenu } from "./buttons/user/user";
 export const Navbar = () => {
   return (
     <>
-      <nav className="flex w-full h-14 p-3 items-center justify-between bg-white dark:bg-neutral-900">
+      <nav className="fixed top-0 z-50 w-full border-b flex justify-center items-center h-14 bg-white dark:bg-neutral-900">
         <section className="flex gap-6 ml-2">
           <MenuButton />
           <Logo />
         </section>
-        <section className="relative flex justify-center items-center gap-4 w-full">
+        <section className=" flex justify-center items-center gap-4 w-full">
           <Search />
           <Mic />
         </section>
