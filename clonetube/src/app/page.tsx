b67694e0e8a0547489fe0ff43aafc1c540c39fb9
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-11 09:22:29 +0000

    readjustment in main page where will take the content

--- clonetube/src/app/page.tsx
+++ clonetube/src/app/page.tsx
@@ -1,11 +1,14 @@
-
+import { VideoFilters } from "@/components/filters/filters";
 
 export default function Home() {
   return (
     <>
-    <section className="flex justify-center items-center ">
-      <h1>Hello</h1>
-    </section>
+      <VideoFilters />
+      <section className="p-4 sm:ml-64 text-white">
+        <section className="p-4 border-2 text-white border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+          Hello Container
+        </section>
+      </section>
     </>
   );
 }
