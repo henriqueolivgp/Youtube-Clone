4ab7b8c1b52a953e766c904aff7b97c02e0e7bdf
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-11 09:23:44 +0000

    change font width in logo component

--- clonetube/src/components/navbar/logo/logo.tsx
+++ clonetube/src/components/navbar/logo/logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
       <Link href="/" className="flex gap-0.5 items-center">
         <Image src="/youtube.png" width={28} height={500} alt="Youtube Logo" />
         <section>
-          <span className="text-white font-semibold tracking-tighter text-xl relative">
+          <span className="text-white font-medium tracking-tighter text-xl relative">
             CloneTube
           </span>
           <span className="text-gray-400 text-[10px] absolute ml-0.5">PT</span>
