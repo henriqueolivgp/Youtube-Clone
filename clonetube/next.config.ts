4674a82b358905e09565ce1ea95db6447f38fa55
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2025-03-11 09:15:21 +0000

    add wepck config for svg's

--- clonetube/next.config.ts
+++ clonetube/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  webpack(config) {
+    // Adiciona o processamento de arquivos .svg
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
